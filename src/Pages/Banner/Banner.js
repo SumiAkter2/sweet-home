e3027d65d48bcb50cf2112da1e5cf2fa2864e132
SumiAkter2 <akteralisha36@gmail.com>
2022-09-26 11:41:26 +0600

    added background img

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -2,17 +2,24 @@ import React from "react";
 import "./Banner.css";
 const Banner = () => {
   return (
-    <div className="Banner-container">
-      <div>
-        <h1>kjhsdorfu alsdhjroe kljdhsro </h1>
-      </div>
-      <div className="banner-img">
-        <img
-          style={{ width: "400px" }}
-          src="https://i.ibb.co/NrnZzgM/room2.png"
-          alt="banner-img"
-        />
-      </div>
+    <div>
+      <div
+        className="Banner-container"
+        style={{
+          backgroundImage: 'url("https://i.ibb.co/1XSs988/room6.png")',
+        }}
+      >
+        <div className="banner-content">
+          <h1> Expect Nothing But The Best </h1>
+          <p>
+            Hotel for the elite passionate about luxury & comfort. Sweet Home is
+            a lavish and comfortable hotels and residences in Dhaka, Bangladesh.
+          </p>
+          <button className="btn ">About More ...</button>
+        </div>
+          </div>
+          
+          {/* something */}
     </div>
   );
 };
